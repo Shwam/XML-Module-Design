57a9ad1a8627530a1259bae61b56ba8202fb3f56
Shawn McCormick <wsmkd6@mst.edu>
2014-11-15 11:44:07 -0600

    Save additional object to xml file

--- XML-Based-Modules/MainWindow.xaml.cs
+++ XML-Based-Modules/MainWindow.xaml.cs
@@ -16,11 +16,16 @@ namespace XML_Based_Modules
             string path = Directory.GetCurrentDirectory() + "../../../";
             XmlDocument xdoc = new XmlDocument();
             xdoc.Load(path + "SampleInput.xml");
-
             string xml = xdoc.InnerXml;
 
             XmlSerializer serializer = new XmlSerializer(typeof(ModularDataEntries));
             ModularDataEntries xi = (ModularDataEntries)serializer.Deserialize(new StringReader(xml));
+
+            
+
+            // Add another entry
+            xi.DataModules.Add(new ModularData("Green Light", 6013, "0 - 255; 0 is off, 255 is full on", "uint8"));
+            InitializeComponent();
 
             foreach (ModularData d in xi.DataModules)
             {
@@ -29,7 +34,10 @@ namespace XML_Based_Modules
                 Console.WriteLine(d.Description);
                 Console.WriteLine(d.DataType);
             }
-            InitializeComponent();
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter(path + "ModuleSave.xml");
+            serializer.Serialize(file, xi);
+            file.Close();
         }
     }
 }
